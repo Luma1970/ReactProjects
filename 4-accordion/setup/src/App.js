381435e70aa29453409472a1626a86390a356d45
Luma <luma@marinellaluft.com>
2020-11-20 18:20:58 +0100

    useEffect e Conditional rendering

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [query, setQuery] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>Domande e risposte sul login</h3>
+        <section>
+          {query.map((demand) => {
+            return (
+              <SingleQuestion key={demand.id} {...demand}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App;
--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ title, info }) => {
+  const [answer, setAnswer] = useState(true);
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => {
+          setAnswer(!answer)
+        }}>
+          {answer ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+      </header>
+      <p>{answer ? null : info}</p>
+    </article>
+  );
 };
 
 export default Question;
