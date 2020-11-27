eea6b6518db90d2bfa886467993ba4f711e1661c
Luma <luma@marinellaluft.com>
2020-11-27 18:23:16 +0100

    Color Generator - Form - Implementare

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -4,7 +4,47 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#6a2aff').all(10)); 
+// il numero tra parentesi indica il numero di tinte e sfumature per cui andrà diviso il 100% quindi più e alto meno campioni si avranno
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+     
+  }
+
+  return (
+    <>
+    <section className="container">
+      <h3>Generatore di colori</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={color}
+          onChange={ (e) => setColor(e.target.value)}
+          placeholder="#6a2aff"
+          className={`${error ? 'error' : null}`}/>
+        <button className="btn" type="submit">
+          invia
+        </button>
+      </form>
+    </section>
+    <section className="colors">
+      {list.map( (color, index) => {
+        console.log(color)
+        return <SingleColor key={index} {...color} index={index} />
+      })}
+    </section>
+    </>
+  )
 }
 
 export default App
--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb, weight, index}) => {
+  const [alert, setAlert] = useState(false)
+  const bckg = rgb.join(',') //trasformo i numeri in stringa e li divido con virgole
+  const hex = rgbToHex(...rgb) //funzione che trasforma rgb in hex vedi utils.js
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [alert])
+  
+  return (
+    <article className={`color ${index > 9 && 'color-light'}`} style={{backgroundColor: `rgb(${bckg})`}}
+    onClick={() => {
+      setAlert(true)
+      navigator.clipboard.writeText(hex) //metodo per copiare gli appunti
+    }}> 
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+      {alert && <p className="alert">copiato negli appunti</p>}
+    </article>
+  ) // non applico un colore semplice al backg ma la funzione che richiama il mio rgb che fa parte dell'oggetto dellarray nell'API
 }
 
 export default SingleColor
