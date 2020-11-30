50b1f13e23c1a01fbbca3f0f63a5ac75a375e465
Luma <luma@marinellaluft.com>
2020-11-30 16:38:27 +0100

    Grocery bud - Forms

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,14 @@
 import React, { useEffect } from 'react'
 
-const Alert = () => {
-  return <h2>alert component</h2>
+const Alert = ({type, msg, removeAlert, list}) => {
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert()
+    }, 3000)
+    return () => clearTimeout(timeout)
+  },[list]) // in questo modo anche se cancello tutto subito dopo un altro avviso, il nuovo avviso resta per 3 secondi perché ho riaggiornato la list
+  return <p className={`alert alert-${type}`}>{msg}</p>
 }
 
 export default Alert
--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,8 +2,93 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if(list) {
+    return JSON.parse(localStorage.getItem('list'))
+  } else {
+    return []
+  }
+}
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState(getLocalStorage())
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: ''})
+
+  const handlesubmit = (e) => {
+    e.preventDefault()
+    if (!name) {
+      showAlert(true, 'danger', 'Inserisci un elemento')
+    } else if (name && isEditing) {
+      setList(
+        list.map((item)=> {
+          if (item.id === editID) {
+            return {...item, title: name}
+          }
+          return item
+        })
+      )
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+      showAlert(true, 'success', 'Modificato con successo')
+    } else {
+      showAlert(true, 'success', 'Elemento aggiunto alla lista')
+      const newItem = {id: new Date().getTime().toString(), title: name}
+      setList([...list, newItem])
+      setName('')
+    }    
+  }
+
+  const showAlert = (show=false, type='', msg='') => {
+    setAlert({show, type, msg})
+  }
+  const clearList = () => {
+    showAlert(true, 'danger', 'Lista vuota')
+    setList([])
+  }
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'Elemento rimosso')
+    setList(list.filter((item) => item.id !== id))
+  }
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id)
+    setIsEditing(true)
+    setEditID(id)
+    setName(specificItem.title)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list]) // salvo in locale gli elementi della lista e ogni volta che che aggiorno la aggiorna
+
+  return (
+    <section className='section-center'>
+      <form className="grocery-form" onSubmit={handlesubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>Lista della spesa</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className='grocery'
+            placeholder='es. uova'
+            value={name}
+            onChange={(e) => setName(e.target.value)}/>
+          <button className="submit-btn" type='submit'>
+            {isEditing ? 'modifica' : 'invia'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+        <List items={list} removeItem={removeItem} editItem={editItem} />
+        <button className="clear-btn" onClick={clearList}>pulisci tutto</button>
+      </div>)
+      }
+    </section>
+  )
 }
 
 export default App
