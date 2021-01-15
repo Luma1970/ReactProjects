6344f73214e5c810581ec8c4a4acb16b03d3c9c8
Luma <luma@marinellaluft.com>
2021-01-15 11:18:22 +0100

    Cocktails - React Router e useCallback

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -4,10 +4,27 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext()
+
+  if (loading) {
+    return <Loading />
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        Non abbiamo cocktail con questo nome, perché non provi a inventarlo?
+      </h2>
+    )
+  }
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+   <section className='section'>
+     <h2 className="section-title">Cocktail</h2>
+     <div className="cocktails-center">
+       {cocktails.map((drink) => {
+         return <Cocktail key={drink.id} {...drink} />
+       })}
+     </div>
+   </section>
   )
 }
 
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className="section-title">
+        Dicono di noi
+      </h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad nulla sunt quas consequuntur similique assumenda temporibus est itaque iure quae corrupti perferendis unde velit quia consectetur vitae, obcaecati quisquam. Molestias?
+      </p>
+    </section>
   )
 }
 
